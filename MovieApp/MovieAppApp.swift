0339c36afca1dfac1739a846f8df606cde429868
Ibrahim diab <ibrahimdiab011@gmail.com>
2025-09-24 06:49:15 +0300

    Initial Commit

---
+++ MovieApp/MovieAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieAppApp.swift
+//  MovieApp
+//
+//  Created by Diab on 24/09/2025.
+//
+
+import SwiftUI
+
+@main
+struct MovieAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
